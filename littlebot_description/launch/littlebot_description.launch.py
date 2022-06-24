8f92095ca8048b3cde22f785e58dbec8570e35e9
NestorDP <nestor-dp@hotmail.com>
2022-06-24 03:29:14 -0300

    create control package

--- littlebot_description/launch/littlebot_description.launch.py
+++ littlebot_description/launch/littlebot_description.launch.py
@@ -10,37 +10,64 @@ from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
+
     description_path = get_package_share_path('littlebot_description')
     urdf_model_path  = description_path / 'urdf/littlebot_description.urdf.xacro'
     rviz_config_path = description_path / 'rviz/littlebot_description.rviz'
+    use_sim_time     = LaunchConfiguration('use_sim_time', default='false')
 
-    gui_arg   = DeclareLaunchArgument(name='gui', default_value='false', choices=['true', 'false'],
-                                      description='Flag to enable joint_state_publisher_gui')
+    # Launch arguments
+    #-----------------------------------------------------------------------------
+    time_arg  = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        choices=['true', 'false'],
+        description='Use simulation (Gazebo) clock if true')
+    
+    joint_gui_arg   = DeclareLaunchArgument(
+        name='joint_gui', 
+        default_value='false', 
+        choices=['true', 'false'],
+        description='Flag to enable joint_state_publisher_gui')
 
-    model_arg = DeclareLaunchArgument(name='model', default_value=str(urdf_model_path),
-                                      description='Absolute path to robot urdf file')
+    model_arg = DeclareLaunchArgument(
+        name='model', 
+        default_value=str(urdf_model_path),
+        description='Absolute path to robot urdf file')
 
-    rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=str(rviz_config_path),
-                                    description='Absolute path to rviz config file')
+    rviz_arg  = DeclareLaunchArgument(
+        name='rvizconfig',
+        default_value=str(rviz_config_path),
+        description='Absolute path to rviz config file')
 
-    robot_description = ParameterValue(Command(['xacro ', LaunchConfiguration('model')]), value_type=str)
+    # Parameters
+    #-----------------------------------------------------------------------------
+    robot_description = ParameterValue(
+        Command(['xacro ', LaunchConfiguration('model')]), 
+        value_type=str)
 
+    # Nodes
+    #-----------------------------------------------------------------------------
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': robot_description}]
+        name='robot_state_publisher',
+        parameters=[
+            {'robot_description': robot_description}, 
+            {'use_sim_time': use_sim_time}],
+        output='screen'
     )
 
     joint_state_publisher_node = Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
-        condition=UnlessCondition(LaunchConfiguration('gui'))
+        condition=UnlessCondition(LaunchConfiguration('joint_gui'))
     )
 
     joint_state_publisher_gui_node = Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
-        condition=IfCondition(LaunchConfiguration('gui'))
+        condition=IfCondition(LaunchConfiguration('joint_gui'))
     )
 
     rviz_node = Node(
@@ -52,7 +79,8 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        gui_arg,
+        time_arg,
+        joint_gui_arg,
         model_arg,
         rviz_arg,
         joint_state_publisher_node,
