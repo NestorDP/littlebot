55163201c0df9c9e741343b45ada993f1565f822
NestorDP <nestor-dp@hotmail.com>
2022-06-27 17:25:29 -0300

    included IMU in simulation

---
+++ littlebot_localization/launch/littlebot_control.launch.py
@@ -0,0 +1,43 @@
+from launch import LaunchContext, LaunchDescription
+from launch.substitutions import EnvironmentVariable, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    lc = LaunchContext()
+    ld = LaunchDescription()
+
+    config_littlebot_ekf = PathJoinSubstitution(
+        [FindPackageShare('littlebot_control'),
+        'config',
+        'localization.yaml'],
+    )
+
+    node_ekf = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_node',
+        output='screen',
+        parameters=[config_littlebot_ekf],
+        )
+    ld.add_action(node_ekf)
+
+    primary_imu_enable = EnvironmentVariable('CPR_IMU', default_value='false')
+
+    if (primary_imu_enable.perform(lc)) == 'true':
+        config_imu_filter = PathJoinSubstitution(
+            [FindPackageShare('littlebot_control'),
+            'config',
+            'imu_filter.yaml'],
+        )
+        node_imu_filter = Node(
+            package='imu_filter_madgwick',
+            executable='imu_filter_madgwick_node',
+            name='imu_filter',
+            output='screen',
+            parameters=[config_imu_filter]
+        )
+        ld.add_action(node_imu_filter)
+
+    return ld
