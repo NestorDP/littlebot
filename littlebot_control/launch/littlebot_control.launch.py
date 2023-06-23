3f44b5dc6a75363872c8dc28f644ba40c4816689
NestorDP <nestor-dp@hotmail.com>
2023-06-23 17:45:07 -0300

    Seting twist mux

--- littlebot_control/launch/littlebot_control.launch.py
+++ littlebot_control/launch/littlebot_control.launch.py
@@ -1,8 +1,37 @@
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch.event_handlers import OnProcessExit
+
+def generate_launch_description():
+
+    filepath_config_twist_mux = PathJoinSubstitution(
+        [FindPackageShare('littlebot_control'), 'config', 'twist_mux.yaml']
+    )
+
+    # filepath_config_interactive_markers = PathJoinSubstitution(
+    #     [FindPackageShare('littlebot_control'), 'config', 'teleop_interactive_markers.yaml']
+    # )
+
+
+    # node_interactive_marker_twist_server = Node(
+    #     package='interactive_marker_twist_server',
+    #     executable='marker_server',
+    #     name='twist_server_node',
+    #     remappings={('cmd_vel', 'twist_marker_server/cmd_vel')},
+    #     parameters=[filepath_config_interactive_markers],
+    #     output='screen',
+    # )
+
+    node_twist_mux = Node(
+        package='twist_mux',
+        executable='twist_mux',
+        output='screen',
+        remappings={('/cmd_vel_out', '/littlebot_velocity_controller/cmd_vel')},
+        parameters=[filepath_config_twist_mux]
+    )
+
+    ld = LaunchDescription()
+    # ld.add_action(node_interactive_marker_twist_server)
+    ld.add_action(node_twist_mux)
+    return ld
