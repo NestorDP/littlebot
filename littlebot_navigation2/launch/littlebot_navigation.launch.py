d4c120a7fc8cfdbfe62cc09e8d914a9d839a9879
NestorDP <nestor-dp@hotmail.com>
2023-06-27 00:01:54 -0300

    Remap odometry and cmd_vel topics name

--- littlebot_navigation2/launch/littlebot_navigation.launch.py
+++ littlebot_navigation2/launch/littlebot_navigation.launch.py
@@ -18,14 +18,16 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, GroupAction, SetEnvironmentVariable
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
+from launch_ros.actions import SetRemap
 
 
 def generate_launch_description():
+    # Configurations
+    #-----------------------------------------
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
 
     map_dir = LaunchConfiguration(
@@ -42,12 +44,11 @@ def generate_launch_description():
             'config',
             'littlebot_navigation2.yaml'))
 
+    # Path to lauch folder of the nav2_bringup package
+    #-----------------------------------------
     nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 
         'launch')
 
-    rviz_config_dir = os.path.join(get_package_share_directory('nav2_bringup'), 
-        'rviz',
-        'nav2_default_view.rviz')
 
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -64,20 +65,16 @@ def generate_launch_description():
             'use_sim_time',
             default_value='false',
             description='Use simulation (Gazebo) clock if true'),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
-            launch_arguments={
-                'map': map_dir,
-                'use_sim_time': use_sim_time,
-                'params_file': param_dir}.items(),
-        ),
-
-        # Node(
-        #     package='rviz2',
-        #     executable='rviz2',
-        #     name='rviz2',
-        #     arguments=['-d', rviz_config_dir],
-        #     parameters=[{'use_sim_time': use_sim_time}],
-        #     output='screen'),
+        
+        GroupAction([
+            SetRemap(src='/cmd_vel', dst='/littlebot_velocity_controller/cmd_vel'),
+            SetRemap(src='/odom', dst='/odometry/filtered'),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
+                launch_arguments={
+                    'map': map_dir,
+                    'use_sim_time': use_sim_time,
+                    'params_file': param_dir}.items(),
+            )
+        ])
     ])
