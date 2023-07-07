db42b37b76d18b7f151d1f20ad68bd6d1bb1e6bf
NestorDP <nestor-dp@hotmail.com>
2023-07-06 23:11:04 -0300

    Added joy_linux_node into littlebot_teleop launch

--- littlebot_teleop/launch/littlebot_teleop.launch.py
+++ littlebot_teleop/launch/littlebot_teleop.launch.py
@@ -2,7 +2,7 @@
 """Launch a talker and a listener in a component container."""
 
 from launch import LaunchDescription
-from launch_ros.actions import ComposableNodeContainer
+from launch_ros.actions import ComposableNodeContainer, Node
 from launch_ros.descriptions import ComposableNode
 
 
@@ -22,7 +22,16 @@ def generate_launch_description():
             output='screen',
     )
 
+    joystick = Node(
+        package='joy_linux',
+        executable='joy_linux_node',
+        name='joystick',
+        output='screen',
+        arguments=['-p dev_name:="/dev/input/js0"']
+        )
+
     ld = LaunchDescription()
     ld.add_action(container)
+    ld.add_action(joystick)
 
     return ld
