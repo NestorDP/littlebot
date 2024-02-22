6932a86754c51a1b39ba78d11b927586bc8af205
aldenpower <eng.andersonfsl@gmail.com>
2024-02-22 05:12:47 -0300

    add rviz2 node to controller launch

--- littlebot_control/launch/spawn_diffdriver_controller.launch.py
+++ littlebot_control/launch/spawn_diffdriver_controller.launch.py
@@ -19,6 +19,13 @@ def generate_launch_description():
         arguments=["littlebot_velocity_controller", "--controller-manager", "/controller_manager"],
     )
 
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        # arguments=["-d", rviz_config_file],
+    )
 
     delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
         event_handler=OnProcessExit(
@@ -26,10 +33,17 @@ def generate_launch_description():
             on_exit=[robot_controller_spawner],
         )
     )
+    delay_rviz_after_robot_controller = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=robot_controller_spawner,
+            on_exit=[rviz_node],
+        )
+    )
 
     return LaunchDescription([
         # control_node,
         joint_state_broadcaster_spawner,
         delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
+        delay_rviz_after_robot_controller
 
     ])
