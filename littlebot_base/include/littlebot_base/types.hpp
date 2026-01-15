c34b2e2c123dc61dff0c335d6a72cc14baacdc94
Nestor Neto <nestor-dp@hotmail.com>
2026-01-15 11:43:51 -0300

    ✨ feat(drive errors): Create new struct and enum to get the communication erros

--- littlebot_base/include/littlebot_base/types.hpp
+++ littlebot_base/include/littlebot_base/types.hpp
@@ -2,6 +2,7 @@
 
 #include <array>
 #include <cstddef>
+#include <cstdint>
 
 namespace littlebot_base
 {
@@ -13,6 +14,33 @@ struct WheelRTData
   std::array<float, kNumWheels> command_velocity{};
   std::array<float, kNumWheels> status_velocity{};
   std::array<float, kNumWheels> status_position{};
+};
+
+enum class DriverError
+{
+  None = 0,
+
+  NoData,             // no frame available yet
+  DecodeFailure,      // Deserialize error
+  EncodeFailure,      // Serialize error
+  SizeMismatch,       // wrong number of wheels
+  SerialReadError,    // IO error
+  SerialWriteError,   // IO error
+};
+
+struct DriverErrorCounters
+{
+  uint64_t no_data = 0;
+  uint64_t decode_failure = 0;
+  uint64_t encode_failure = 0;
+  uint64_t size_mismatch = 0;
+  uint64_t serial_read_error = 0;
+  uint64_t serial_write_error = 0;
+
+  void reset()
+  {
+    *this = {};
+  }
 };
 
 }  // namespace littlebot_base
